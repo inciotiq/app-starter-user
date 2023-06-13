143841154da5177e75c3d51ae4f1b15dd45ed9c5
Oguz Kurt <oguz@iotiq.net>
2023-06-13 15:10:22 +0300

    Add TestContainers for user module integration tests.

--- src/main/java/com/iotiq/user/internal/UserService.java
+++ src/main/java/com/iotiq/user/internal/UserService.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.UUID;
 
 import static com.iotiq.commons.util.NullHandlerUtil.setIfNotNull;
--- src/test/java/com/iotiq/user/UserApplicationTests.java
+++ src/test/java/com/iotiq/user/UserApplicationTests.java
@@ -1,13 +1,47 @@
 package com.iotiq.user;
 
+import com.iotiq.user.domain.Role;
+import com.iotiq.user.messages.UserCreateDto;
+import com.iotiq.user.messages.UserDto;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-//@SpringBootTest
+import static org.junit.Assert.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class UserApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:15-alpine");
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    @Order(1)
+    void add() {
+        UserCreateDto userCreateDto = new UserCreateDto();
+        userCreateDto.setEmail("email@email.com");
+        userCreateDto.setFirstname("First");
+        userCreateDto.setLastname("Last");
+        userCreateDto.setUsername("firstlast");
+        userCreateDto.setPassword("pass");
+        userCreateDto.setRole(Role.ADMIN);
+
+        UserDto createdUser = restTemplate
+                .postForObject("/api/v1/users", userCreateDto, UserDto.class);
+        assertNotNull(createdUser);
+        assertEquals("email@email.com", createdUser.getEmail());
+    }
 }
