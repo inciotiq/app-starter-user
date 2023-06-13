580026048a259060d528b38842222ba1883cb010
inci <inci@iotiq.net>
2023-06-13 11:38:58 +0300

    remove maven plugin from user and commons

--- src/test/java/com/iotiq/user/UserApplicationTests.java
+++ src/test/java/com/iotiq/user/UserApplicationTests.java
@@ -3,11 +3,11 @@ package com.iotiq.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UserApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
