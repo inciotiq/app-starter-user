f70065fabb493cd2c482764da764efa47f3cdb62
inci <inci@iotiq.net>
2023-06-14 11:37:56 +0300

    add user creation changes

--- src/main/java/com/iotiq/user/UserController.java
+++ src/main/java/com/iotiq/user/UserController.java
@@ -2,15 +2,20 @@ package com.iotiq.user;
 
 import com.iotiq.commons.message.response.PagedResponse;
 import com.iotiq.commons.message.response.PagedResponseBuilder;
+import com.iotiq.commons.util.HeaderUtil;
 import com.iotiq.user.domain.User;
 import com.iotiq.user.internal.UserService;
 import com.iotiq.user.messages.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,6 +25,9 @@ import java.util.UUID;
 public class UserController {
 
     private final UserService userService;
+
+    @Value("${application.name}")
+    private String applicationName;
 
     @GetMapping
 //    @PreAuthorize("hasAuthority(@UserManagementAuth.VIEW)")
@@ -37,11 +45,26 @@ public class UserController {
         return UserDto.of(user);
     }
 
+    /**
+     * {@code POST  /users}  : Creates a new user.
+     * <p>
+     * Creates a new user if the login and email are not already used, and sends an
+     * mail with an activation link.
+     * The user needs to be activated on creation.
+     *
+     * @param userDTO the user to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new user, or with status {@code 400 (Bad Request)} if the login or email is already in use.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     * @throws BadRequestAlertException {@code 400 (Bad Request)} if the login or email is already in use.
+     */
     @PostMapping
 //    @PreAuthorize("hasAuthority(@UserManagementAuth.CREATE)")
-    public UserDto create(@RequestBody @Valid UserCreateDto request) {
+    public ResponseEntity<UserDto> create(@RequestBody @Valid UserCreateDto request) throws URISyntaxException {
         User user = userService.create(request);
-        return UserDto.of(user);
+        return ResponseEntity
+                .created(new URI("/api/v1/users/" + user.getId()))
+                .headers(HeaderUtil.createAlert(applicationName, "userManagement.created", String.valueOf(user.getId())))
+                .body(UserDto.of(user));
     }
 
     @PutMapping("/{id}")
