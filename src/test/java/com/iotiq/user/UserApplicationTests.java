5e8b1a324ac6c32ae8130cedd890f12958a6ec6d
inci <inci@iotiq.net>
2023-06-14 11:56:50 +0300

    update user tests

--- src/test/java/com/iotiq/user/UserApplicationTests.java
+++ src/test/java/com/iotiq/user/UserApplicationTests.java
@@ -4,13 +4,18 @@ import com.iotiq.user.domain.Role;
 import com.iotiq.user.domain.User;
 import com.iotiq.user.internal.UserRepository;
 import com.iotiq.user.messages.UserCreateDto;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -19,16 +24,25 @@ import java.util.List;
 import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @AutoConfigureMockMvc
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 class UserApplicationTests {
 
+    private static String id;
+    String MAIL = "email@email.com";
+    String FIRSTNAME = "First";
+    String LASTNAME = "Last";
+    String USERNAME = "firstlast";
+    String PASSWORD = "pass";
 
     @Container
     @ServiceConnection
@@ -42,15 +56,11 @@ class UserApplicationTests {
     private UserRepository userRepository;
 
     @Test
+    @Order(1)
     void add() throws Exception {
         int databaseSizeBeforeCreate = userRepository.findAll().size();
 
         UserCreateDto userCreateDto = new UserCreateDto();
-        String MAIL = "email@email.com";
-        String FIRSTNAME = "First";
-        String LASTNAME = "Last";
-        String USERNAME = "firstlast";
-        String PASSWORD = "pass";
 
         userCreateDto.setEmail(MAIL);
         userCreateDto.setFirstname(FIRSTNAME);
@@ -59,11 +69,16 @@ class UserApplicationTests {
         userCreateDto.setPassword(PASSWORD);
         userCreateDto.setRole(Role.ADMIN);
 
-        mockMvc
-                .perform(
-                        post("/api/v1/users").contentType(MediaType.APPLICATION_JSON)
-                                .content(TestUtil.convertObjectToJsonBytes(userCreateDto))
-                ).andExpect(status().isOk());
+        ResultActions result = mockMvc.perform(
+                post("/api/v1/users").contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtil.convertObjectToJsonBytes(userCreateDto))
+        );
+
+        result.andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", notNullValue()));
+
+        id = JsonPath.read(result.andReturn().getResponse().getContentAsString(), "$.id");
 
         assertPersistedUsers(users -> {
             assertThat(users).hasSize(databaseSizeBeforeCreate + 1);
@@ -73,6 +88,28 @@ class UserApplicationTests {
             assertThat(testUser.getPersonalInfo().getLastName()).isEqualTo(LASTNAME);
             assertThat(testUser.getAccountSecurity().getRole()).isEqualTo(Role.ADMIN);
         });
+    }
+
+    @Test
+    @Order(2)
+    void findById() throws Exception {
+        int databaseSizeBeforeCreate = userRepository.findAll().size();
+
+        assertThat(databaseSizeBeforeCreate).isEqualTo(1);
+
+        ResultActions result = mockMvc
+                .perform(
+                        get("/api/v1/users/" + id)
+                );
+
+        result.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpectAll(
+                        jsonPath("$.id", is(id)),
+                        jsonPath("$.firstname", is(FIRSTNAME)),
+                        jsonPath("$.lastname", is(LASTNAME)),
+                        jsonPath("$.username", is(USERNAME))
+                );
     }
 
     private void assertPersistedUsers(Consumer<List<User>> userAssertion) {
