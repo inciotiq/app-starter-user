811966274cfbd6e1736bdf38294de2c322992a1c
inci <inci@iotiq.net>
2023-06-16 16:15:56 +0300

    extend UserNotFoundException from ApplicationException

--- src/main/java/com/iotiq/user/exceptions/UserNotFoundException.java
+++ src/main/java/com/iotiq/user/exceptions/UserNotFoundException.java
@@ -1,9 +1,12 @@
 package com.iotiq.user.exceptions;
 
-import com.iotiq.commons.exceptions.EntityNotFoundException;
+import com.iotiq.commons.exceptions.ApplicationException;
+import org.springframework.http.HttpStatus;
 
-public class UserNotFoundException extends EntityNotFoundException {
-    public UserNotFoundException(Object... args) {
-        super("user", args);
+import java.util.Collections;
+
+public class UserNotFoundException extends ApplicationException {
+    public UserNotFoundException() {
+        super(HttpStatus.NOT_FOUND, "userNotFound", Collections.emptyList());
     }
 }
